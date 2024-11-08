eb73956083da36a34ece0ca2e1d8564f5482c790
pvictorianizzaro713 <paulanizzaro85@gmail.com>
2024-11-08 15:26:40 -0300

    calculadora

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+export default function App() {
+    return (
+        <main>
+            <h1>Calculadora</h1>
+            <input type="number" placeholder="Insira um número"/>
+            <input type="number" placeholder="Insira um número"/>
+       
+       <div>
+        <button>+</button>
+        <button>-</button>
+        <button>/</button>
+        <button>x</button>
+       </div>
+       
+       
+        </main>
+
+    )
+}
