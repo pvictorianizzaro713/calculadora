7aecff36d688f63178e4d67be0d4f507f844935a
pvictorianizzaro713 <paulanizzaro85@gmail.com>
2024-11-22 15:34:18 -0300

    logica calculadora

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,46 @@
+import { useState } from "react"
+
 export default function App() {
-    return (
+    const capturandoPrimeiroValor = (e) => {
+        setPrimeiroValor(Number(e.target))
+        console.log(e.target.value)
+       }
+     
+       const capturandoSegundoValor = (e) => {
+         setSegundoValor(Number(e.target.value))
+       }
+
+       }
+  
+    const soma = () => {
+        setResultado(primeirovalor + segundovalor)
+    }
+   const subtracao = () => {
+    setResultado(primeirovalor- segundovalor)
+   }
+   const divisao = () => {
+    setResultado(primeirovalor / segundovalor )
+   }
+   const multiplicacao = () => {
+    setResultado(primeirovalor *segundovalor)
+   }
+   
+   return (
         <main>
             <h1>Calculadora</h1>
+           
             <input type="number" placeholder="Insira um número"/>
+           
             <input type="number" placeholder="Insira um número"/>
        
        <div>
-        <button>+</button>
-        <button>-</button>
-        <button>/</button>
-        <button>x</button>
+        <button onClick={soma}>+</button>
+        <button onClick={subtracao}>-</button>
+        <button onClick={divisao}>/</button>
+        <button onClick={multiplicacao}>x</button>
        </div>
-       
+       <h3>{resultado}</h3>
        
         </main>
 
     )
-}
